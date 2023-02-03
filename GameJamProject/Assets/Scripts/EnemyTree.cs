c7539f97a5156b6771d8696b6fdd04a2e9897ba2
Nirolf Ucast <72085809+lFirsl@users.noreply.github.com>
2023-02-03 22:23:17 +0000

    Modify enemy script to look at player.

--- GameJamProject/Assets/Scripts/EnemyTree.cs
+++ GameJamProject/Assets/Scripts/EnemyTree.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class EnemyTree : MonoBehaviour
 {
+    
+    private PlayerController Player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GameObject.FindObjectOfType<PlayerController> ();
     }
 
     // Update is called once per frame
-    void Update()
+    void Update ()
     {
-        
+        Vector3 lookAt = Player.transform.position;
+        lookAt.y = transform.position.y;
+        transform.LookAt(lookAt);
     }
 }
