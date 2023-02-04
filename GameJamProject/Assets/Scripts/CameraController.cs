ab6d1a85c708db2d658e6dba4a62391d1e8690f6
MIkha Walter Rohrs <mikharohrs@gmail.com>
2023-02-04 12:08:56 +0000

    Modified camera angle
    Camera now looks down at a 60 degree angle at the ground

--- GameJamProject/Assets/Scripts/CameraController.cs
+++ GameJamProject/Assets/Scripts/CameraController.cs
@@ -6,17 +6,15 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform playerPosition;
-    public float cameraHeight;
+    public float cameraHeight = 25;
+    public Quaternion cameraAngle = Quaternion.Euler(65, 0, 0);
+    public float zOffset = 10;
 
-    private void Start()
-    {
-        // Make camera face down
-        transform.forward = Vector3.down;
-    }
-    
     void Update()
     {
+        // Make camera look down ad an angle, not straight down.
+        transform.rotation = cameraAngle;
         // Make camera follow the player
-        transform.position = playerPosition.transform.position + new Vector3(0, cameraHeight, 0);
+        transform.position = playerPosition.transform.position + new Vector3(0, cameraHeight, -zOffset);
     }
 }
