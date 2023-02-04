ddf6a8d334d3d07a777f7e4ce22ca75014195ac2
MIkha Walter Rohrs <mikharohrs@gmail.com>
2023-02-04 15:30:33 +0000

    Finished dialogue box stuff

--- GameJamProject/Assets/Scripts/DialogueScripts/DialogueManager.cs
+++ GameJamProject/Assets/Scripts/DialogueScripts/DialogueManager.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
     private Queue<string> _sentences;
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+    //public Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -14,18 +21,40 @@ public class DialogueManager : MonoBehaviour
 
     public void StartDialogue(Dialogue dialogue)
     {
+        nameText.text = dialogue.name;
         
+        _sentences.Clear();
+
+        foreach (var sentence in dialogue.sentences)
+        {
+           _sentences.Enqueue(sentence); 
+        }
+        
+        DisplayNextSentence();
     }
 
     public void DisplayNextSentence()
     {
-        
+        if (_sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        var sentence = _sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
     }
 
-    // IEnumerator TypeSentence(string sentence)
-    // {
-    //     
-    // }
+    private IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (var character in sentence.ToCharArray())
+        {
+            dialogueText.text += character;
+            yield return null;
+        }
+    }
 
     private void EndDialogue()
     {
--- GameJamProject/Assets/Scripts/DialogueScripts/DialogueTrigger.cs
+++ GameJamProject/Assets/Scripts/DialogueScripts/DialogueTrigger.cs
@@ -1,10 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+
+    public GameObject player;
+
+    private void Update()
+    {
+        var playerPosition = player.transform.position;
+        var distance = Vector3.Distance(playerPosition, transform.position);
+        
+        if (distance <= 10 && Input.GetKey(KeyCode.Return))
+        {
+            TriggerDialogue();
+        }
+    }
 
     public void TriggerDialogue()
     {
