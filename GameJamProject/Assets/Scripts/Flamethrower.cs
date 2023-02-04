23d9300bd912a6ff443ec53f25c3157ff75f4a48
Liam <60526247+The-Liam-Blair@users.noreply.github.com>
2023-02-04 12:05:37 +0000

    Added Flamethrower Firing, Updated Sprite
    - Flamethrower can now be fired by holding the spacebar.
    - Updated flamethrower colouring and sprite (it looks better now :) )

--- GameJamProject/Assets/Scripts/Flamethrower.cs
+++ GameJamProject/Assets/Scripts/Flamethrower.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,24 +8,51 @@ public class Flamethrower : MonoBehaviour
 {
 
     private ParticleSystem ps;
-    [SerializeField] private float flameMaxRange;
-    private Vector3 startPos;
+    [SerializeField] private float flameMaxRange; // Particles are destroyed beyond this range.
+    private Vector3 emitterPos; // Position of the flamethrower base, used to range check vs particle positions.
+    private bool isFiring;
 
     private void Start()
     {
-        startPos = transform.position;
+        emitterPos = transform.position;
         ps = GetComponent<ParticleSystem>();
         flameMaxRange = 40f;
     }
 
     private void Update()
     {
-        ParticleSystem.Particle[] partSystem = new ParticleSystem.Particle[ps.particleCount];
-        int numParticlesAlive = ps.GetParticles(partSystem);
-
-        ParticleSystem.Particle[] distanceParticles = partSystem.Where(p => Vector3.Distance(startPos, p.position) < flameMaxRange).ToArray();
+        ParticleSystem.Particle[] partSystem = new ParticleSystem.Particle[ps.particleCount]; // Get array of particles within the particle system.
+        int numParticlesAlive = ps.GetParticles(partSystem);  // Get number of currently active particles.
         
-        ps.SetParticles(distanceParticles, distanceParticles.Length);
+        ParticleSystem.Particle[] validParticles = GetInRangeParticles(partSystem);
+        
+        ps.SetParticles(validParticles, validParticles.Length); // Set particle system to use particles in arrays, aka omit particles that were outside the range.
+
+        isFiring = Input.GetKey(KeyCode.Space); // If using primary fire key, set firing to true, otherwise false. 
+
+        // Shoot flamethrower if firing.
+        if (isFiring) { ps.Play(); }
+        else { ps.Stop(); }
     }
 
+    /// <summary>
+    /// For each active particle in the flamethrower, check if it's exceeded the flamethrower's max range. Exclude out of range particles.
+    /// </summary>
+    /// <param name="partSystem">Particle system that controls the flamethrower particles.</param>
+    /// <returns>Array of particles within the range threshold.</returns>
+    private ParticleSystem.Particle[] GetInRangeParticles(ParticleSystem.Particle[] partSystem)
+    {
+        List<ParticleSystem.Particle> validParticles = new List<ParticleSystem.Particle>();
+        
+        // For each particle in the array...
+        for (int i = 0; i < partSystem.Length; i++)
+        {
+            // If the particle is within range, add it to the array.
+            if (Vector3.Distance(emitterPos, partSystem[i].position) < flameMaxRange)
+            {
+                validParticles.Add(partSystem[i]);
+            }
+        }
+        return validParticles.ToArray();
+    }
 }
